5aee8b2a346270d9ef5919427035ecc0bac01f45
Hori,Masaki <masakih@hm.h555.net>
2010-01-15 13:50:24 +0000

    [Mod] 64bitに対応。
    git-svn-id: svn+ssh://macmini/usr/local/svnrepos/HMXSPFcomponent/HMXSPFcomponent@6 d14219da-adb5-4fa0-817f-b5ae090b84d9

--- XspfQTPlaceholderComponent.m
+++ XspfQTPlaceholderComponent.m
@@ -45,9 +45,9 @@ static XspfQTPlaceholderComponent *sharedInstance = nil;
     return self;
 }
 
-- (unsigned)retainCount
+- (NSUInteger)retainCount
 {
-    return UINT_MAX;  //denotes an object that cannot be released
+    return NSUIntegerMax;  //denotes an object that cannot be released
 }
 
 - (void)release
